17f9d3147087909d2681f7d7c361bfa986c5ecfa
Agustin2211 <72230884+Agustin2211@users.noreply.github.com>
2020-10-01 19:55:42 -0300

    Se agrego la funcionalidad de poder exportar a PDF

--- colab.php
+++ colab.php
@@ -7,21 +7,34 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Colaborador</title>
-    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
-    <h1>Colaborador</h1>
-    <form>
-        <input type="buttom" value="Ingresar Asiento" OnClick = "location.href='asientoContable.php'">
-    </form>
-    <form>
-        <input type ="buttom" value="Cerrar Sesion" OnClick= "location.href='logout.php'">
-    </form>
-</body>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Colaborador</title>
+        <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    
+    <body>
+        <h1>Colaborador</h1>
+    
+        <form>
+            <input type="buttom" value="Ingresar Asiento" OnClick = "location.href='asientoContable.php'">
+        </form>
+
+        <form>
+            <input type="buttom" value="Ver Libro Diario" onclick = "location.href='librodiario.php'">
+        </form>
+
+        <form>
+		    <input type="buttom" value="Ver Libro Mayor" OnClick= "location.href= 'libromayor.php'">
+	    </form>
+
+        <form>
+            <input type ="buttom" value="Cerrar Sesion" OnClick= "location.href='logout.php'">
+        </form>
+    </body>
+
 </html>
